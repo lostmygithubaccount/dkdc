559b8faa645b0ba823323c3daea25cc606f0340b
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2019-12-05 14:11:46 -0800

    update whitelist in cleanup script

--- azure/cleanup.py
+++ azure/cleanup.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 whitelist = [
             'copetersRG',
+            'balapv-rg',
+            'balapv-adb-rg',
+            'balapv-synapse-rg'
 ]
 
 df = pd.read_csv('data/Azureresourcegroups.csv')
@@ -12,6 +15,3 @@ for name in names:
     if name not in whitelist:
         print('Deleting: {}'.format(name))
         #subprocess.run(['az', 'group', 'delete', '-n', name, '--no-wait', '-y'])
-
-#subprocess.run(['az', 'group', 'delete', '-n', 'rajeshn-AML-rg', '--no-wait', '-y'])
-#os.system('az group delete -n rajeshndriftrg --no-wait -y')
