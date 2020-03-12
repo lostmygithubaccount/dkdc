b05c3fea8d18d117a5dbda12e1e9d9a6d24e8531
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2020-03-11 21:15:31 -0700

    cleanup

--- delete_rgs.py
+++ delete_rgs.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import pandas as pd
 
-to_delete = ['cody-azureml-rg']
+to_delete = ['cody-westus-rg']
 
 for rg in to_delete:
     print('Deleting: {}'.format(rg))
--- setup.py
+++ setup.py
@@ -9,15 +9,16 @@ from azureml.core import Workspace
 ################################################################################ 
 subscription_id = '6560575d-fa06-4e7d-95fb-f962e74efd7a'
 workspace_name  = 'AzureML'
-locations       = ['westus', 
-                   'westus2', 
-                   'eastus', 
+locations       = ['eastus', 
                    'eastus2', 
+                   'westus', 
+                   'westus2', 
                    'westeurope', 
                    'uksouth', 
                    'southcentralus', 
                    'northcentralus', 
                    'eastuseuap']
+
 # Functions
 ################################################################################ 
 ################################################################################ 
@@ -28,16 +29,16 @@ def setup_workspaces():
 
         ws = Workspace.create(workspace_name, 
                  subscription_id = subscription_id, 
-                 resource_group  = f'{resource_group}',
+                 resource_group  = resource_group,
                  location        = location,
                  )
 
         ws.write_config(file_name = f'{location}.json')
 
-        cmd = 'az network vnet create '                       + \
-              '--name wifi '                                  + \
-              '--resource-group {resource_group}-{location} ' + \
-              '--subnet-name 5GHz'
+        cmd = f'az network vnet create '            + \
+              f'--name wifi '                       + \
+              f'--resource-group {resource_group} ' + \
+              f'--subnet-name 5GHz'
 
         os.system(cmd)
 
