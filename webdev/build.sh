14c7317c4a3ed7f8ac19a52ce0a4a23e6a602ac1
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2020-03-21 21:08:05 -0700

    Create build.sh

---
+++ webdev/build.sh
@@ -0,0 +1,7 @@
+BACKGROUND='http://getwallpapers.com/wallpaper/full/9/b/6/1198446-beautiful-4k-galaxy-wallpaper-3840x2160-ios.jpg'
+THEME='dark'
+USERNAME='lostmygithubaccount'
+LINKEDIN='codydkdc'
+TWITTER='codyDKDC'
+
+gitfolio build $USERNAME --background $BACKGROUND --theme $THEME --linkedin $LINKEDIN --twitter $TWITTER
