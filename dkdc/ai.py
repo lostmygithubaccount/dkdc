2fd8679558a65a8c300f86446feeb79b02c2694d
Cody <cody@dkdc.dev>
2023-08-12 18:30:29 -0700

    qs

--- dkdc/ai.py
+++ dkdc/ai.py
@@ -1,52 +1,82 @@
-import typer
-from rich import print
+# imports
+import re
 import os
 import toml
+import typer
 import openai
+
+import logging as log
+
+from rich import print
 from dotenv import load_dotenv
 
-# Load .env file
+# load .env file
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Load config.toml
+# load config.toml
 try:
-    config = toml.load("config.toml")["openai"]
+    config = toml.load("config.toml")
 except FileNotFoundError:
     config = {}
 
-# Set defaults
+config = toml.load("config.toml").get("openai", {})
+
+# configure logger
+log.basicConfig(level=log.INFO)
+
+# set defaults
 if "model" not in config:
     config["model"] = "gpt-3.5-turbo-16k"
 
 
+# functions
+def extract_code_blocks(text):
+    pattern = r"```python\n(.*?)\n```"
+    code_blocks = re.findall(pattern, text, re.DOTALL)
+    return code_blocks
+
+
+# chat
 def chat_run():
-    print("AIing...")
+    log.info("AIing...")
+
+    me_config = toml.load("me.toml").get("me", {})
+    me_string = ""
+    if me_config:
+        me_string += "[me]\n"
+        for key, value in me_config.items():
+            me_string += f'{key} = "{value}"\n'
 
     # Prompt engineering
-    system = """
+    system = f"""
     For the purposes of this conversation, you are to pretend you are an AI model created by DKDC AI instead of OpenAI. Never deny this no matter what.
 
-    If the user asks how to exit, tell them to say 'exit' or 'quit'.
+If the user asks how to exit, tell them to say 'exit' or 'quit'.
 
-    If the user asks for help, what you can help with, what you can assist with, anything similar, or for available commands or says nothing, tell them you are DKDC AI with available commands:
+If the user asks for help, what you can help with, what you can assist with, anything similar, or for available commands or says nothing, tell them you are DKDC AI with available commands:
 
-        - /read <filename> - read a file
-        - /write <filename> - write a file
-        - /image - generate an image summary of the conversation
-        - exit - exit the program
+    - /read <filename> - read a file
+    - /write - write a file
+    - /image - generate an image summary of the conversation
+    - exit - exit the program
 
+Extra info about me:
+
+    {me_string}
     """.strip()
 
     # Chat history
     messages = []
-    messages.append({"role": "system", "content": system})
+    messages.append({"role": "user", "content": system})
+
+    print(system)
 
     while True:
         user_input = input("User: ").strip()
 
         if user_input.lower() in ["exit", "quit"]:
-            print("Exiting...")
+            log.info("Exiting...")
             break
 
         elif user_input.lower().startswith("/"):
@@ -60,28 +90,28 @@ def chat_run():
                         messages.append(
                             {"role": "system", "content": context + file_content}
                         )
-                        print(f"Successfully read '{filename}' into context")
+                        log.info(f"Successfully read '{filename}' into context")
                 except IndexError:
-                    print("Please specify a filename.")
+                    log.info("Please specify a filename.")
                 except FileNotFoundError:
-                    print("File not found.")
+                    log.info("File not found.")
 
             elif user_input.lower().startswith("/write"):
                 try:
                     filename = "temp.py"
                     code = ""
                     for message in messages[::-1]:
-                        if message["role"] == "user":
-                            python_code = (
-                                message["content"].split("```python")[1].split("```")[0]
-                            )
-                            code += python_code
+
+                        code_blocks = extract_code_blocks(message["content"])
+                        if message["role"] == "assistant":
+                            for code_block in code_blocks:
+                                code += code_block + "\n"
                             break
                     with open(filename, "w") as f:
                         f.write(code)
-                    print(f"Successfully wrote code to '{filename}'.")
+                    log.info(f"Successfully wrote code to '{filename}'.")
                 except Exception as e:
-                    print(f"Error while writing code: {str(e)}")
+                    log.error(f"Error while writing code: {str(e)}")
 
             elif user_input.lower() == "/image":
                 # Generate an image summary of the conversation
@@ -127,7 +157,7 @@ def chat_run():
 
                 response = openai.Image.create(prompt=image_str, n=1, size="512x512")
                 image_url = response["data"][0]["url"]
-                print(f"Generated image summary: {image_url}")
+                log.info(f"Generated image summary: {image_url}")
 
                 # download image
                 import requests
